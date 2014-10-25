9668db17c0dcf60a1fcf627604a7be4a12cab038
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2014-10-25 23:13:38 +0800

    refactor codes

--- lib/token_postman.rb
+++ lib/token_postman.rb
@@ -4,6 +4,20 @@ module TokenPostman
 
     base.class_eval do
 
+      def _postman_login user
+        if params[:cookie]
+          ensure_method_exists(user, :generate_web_access_token)
+          cookies['TOKEN_POSTMAN'] = user.generate_web_access_token
+          render :json => user
+        else
+          ensure_method_exists(user, :generate_access_token)
+          user_hash = user.as_json.merge(
+                        :access_token => user.generate_access_token)
+          render :json => user_hash
+        end
+      end
+      private :_postman_login
+
       def ensure_method_exists(user, method_name)
         unless user.respond_to?(method_name)
           raise MethodNotImplementedException, 
@@ -11,6 +25,18 @@ module TokenPostman
         end
       end
       private :ensure_method_exists
+
+      def _postman_conflict
+        render :json => { :reason => 'login failed' },
+               :status => 409
+      end
+      private :_postman_conflict
+
+      def _postman_bad_request e
+        render :json => { :reason => e.message },
+               :status => 400
+      end
+      private :_postman_bad_request
 
     end
 
@@ -27,30 +53,17 @@ module TokenPostman
 
         instance_method_name = options[:with]
 
-
         define_method instance_method_name do
           begin
             user = user_class.login(params.require(:account), params.require(:validator))
 
             if user
-              if params[:cookie]
-                ensure_method_exists(user, :generate_web_access_token)
-                cookies['TOKEN_POSTMAN'] = user.generate_web_access_token
-                render :json => user
-              else
-                ensure_method_exists(user, :generate_access_token)
-                user_hash = user.as_json.merge(
-                              :access_token => user.generate_access_token)
-                render :json => user_hash
-              end
+              _postman_login(user)
             else
-              render :json => { :reason => 'login failed' },
-                     :status => 409
+              _postman_conflict
             end
-
           rescue ActionController::ParameterMissing => e
-            render :json => { :reason => e.message },
-                   :status => 400
+            _postman_bad_request(e)
           end
         end
       end
